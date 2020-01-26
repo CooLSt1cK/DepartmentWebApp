bcde48e16d8060168fa2eb31b54995a9099ab6ed
Kostiantyn Aleksieienko <00gevill00@gmail.com>
2020-01-26 23:52:37 +0200

    Add constant classes Paths and PropertyNames

---
+++ src/main/java/com/aleksieienko/department/web/app/Paths.java
@@ -0,0 +1,25 @@
+package com.aleksieienko.department.web.app;
+
+public class Paths {
+    public static final String DB_SETTINGS = "src/main/resources/db_settings.property";
+
+    public static final String ADD_DEPARTMENT_JSP = "/WEB-INF/jsp/department/addDepartment.jsp";
+    public static final String DELETE_DEPARTMENT_JSP = "/WEB-INF/jsp/department/deleteDepartment.jsp";
+    public static final String UPDATE_DEPARTMENT_JSP = "/WEB-INF/jsp/department/updateDepartment.jsp";
+    public static final String DEPARTMENT_SERVLET = "/DepartmentServlet";
+
+    public static final String ERROR_PAGE_JSP = "/WEB-INF/jsp/error_page.jsp";
+    public static final String ERROR_CONTROLLER = "/ErrorPage";
+
+    public static final String INDEX_JSP = "/index.jsp";
+
+    public static final String EMPLOYEE_JSP = "/WEB-INF/jsp/employee/employee.jsp";
+    public static final String ADD_EMPLOYEE_JSP = "/WEB-INF/jsp/employee/addEmployee.jsp";
+    public static final String DELETE_EMPLOYEE_JSP = "/WEB-INF/jsp/employee/deleteEmployee.jsp";
+    public static final String EMPLOYEE_SERVLET = "/EmployeeServlet";
+    public static final String UPDATE_EMPLOYEE_JSP = "/WEB-INF/jsp/employee/updateEmployee.jsp";
+
+
+    private Paths() {
+    }
+}
---
+++ src/main/java/com/aleksieienko/department/web/app/PropertyNames.java
@@ -0,0 +1,10 @@
+package com.aleksieienko.department.web.app;
+
+public class PropertyNames {
+    public static final String DB_SETTING_URL = "db.url";
+    public static final String DB_SETTING_USER = "db.user";
+    public static final String DB_SETTING_PASSWORD = "db.password";
+
+    private PropertyNames() {
+    }
+}
