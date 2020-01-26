c3c5b619e93418c98f58594c11048660b2a28ab5
Kostiantyn Aleksieienko <00gevill00@gmail.com>
2020-01-26 23:54:05 +0200

    Add constant class AttributeNames in new package - web

---
+++ src/main/java/com/aleksieienko/department/web/app/web/AttributeNames.java
@@ -0,0 +1,17 @@
+package com.aleksieienko.department.web.app.web;
+
+public class AttributeNames {
+    public static final String DEPARTMENT_SERVICE = "departmentService";
+    public static final String DEPARTMENT_LIST = "departmentList";
+    public static final String DEPARTMENT_BY_ID = "departmentById";
+    public static final String DEPARTMENT_ID = "departmentId";
+
+    public static final String EMPLOYEE_SERVICE = "employeeService";
+    public static final String EMPLOYEE_LIST = "employeeList";
+    public static final String EMPLOYEE_BY_ID = "employeeById";
+
+    public static final String ERROR_MESSAGE = "errorMessage";
+
+    private AttributeNames() {
+    }
+}
